036f08ec822de734c46e1bcf177837a167742248
Shishir Sonekar <shishir@ivs.tv>
2024-08-11 18:09:08 +0530

    basepath

--- next.config.js
+++ next.config.js
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
 	output: 'export',
-	basePath: "/pm-portfolio-1",
+	basePath: "/",
 	reactStrictMode: true,
 	swcMinify: true,
 	images: {
