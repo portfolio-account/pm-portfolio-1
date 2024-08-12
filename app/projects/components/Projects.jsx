364c5ad1a79a142cc0286469481fd7b0b89b908e
Shishir Sonekar <shishir@ivs.tv>
2024-08-12 17:40:53 +0530

    map func

--- app/projects/components/Projects.jsx
+++ app/projects/components/Projects.jsx
@@ -4,7 +4,7 @@ export function Projects({ projects }) {
 	return (
 		<div className="grid grid-cols-1 sm:grid-cols-3 gap-8">
 			{projects
-				?.sort((a, b) => b.index - a.index)
+				?.sort((a, b) => a.index - b.index)
 				?.map((project, index) => (
 					<ProjectItem key={project.index} project={project} index={index} />
 				))}
