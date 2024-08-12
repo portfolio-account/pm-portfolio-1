a73bc2f0e70e5e13513a5cd1ff7f5f899957fcc9
Shishir Sonekar <shishir@ivs.tv>
2024-08-12 17:34:22 +0530

    changed the order

--- app/projects/layout.js
+++ app/projects/layout.js
@@ -19,17 +19,17 @@ export default function Page() {
 	// const { data, error } = useSWR(fetchUrl, fetcher);
 	const data = [
 		{
-			index: 1,
+			index: 7,
 			title: "HyperLite and Hyperpro Mapping Device",
 			description: "Both are mapping devices that can collect data in the form of LiDAR data and SV data, providing centimeter-level accuracy for mapping purposes."
 		},
 		{
-			index: 2,
+			index: 6,
 			title: "Roadmentor Marketplace",
 			description: "You can buy the latest LiDAR, video, and image data from San Francisco, with plans to add new cities soon. This service is owned by Hyperspec AI."
 		},
 		{
-			index: 3,
+			index: 5,
 			title: "Roadmentor AI and ML Tools",
 			description: "Roadmentor Marketplace is useful for processing ROS 1 and ROS 2 data. It enables automatic creation of bounding box annotations, road markings, and model building. Additionally, it supports data segmentation and allows for exporting data for various other uses. This service is owned by Hyperspec AI."
 		},
@@ -39,17 +39,17 @@ export default function Page() {
 			description: "Civil Maps offers a unique and robust approach to real-time localization and navigation with our lightweight Fingerprint Base Map. I have delivered HD maps for various cities in Germany and the USA. This service is now owned by Luminar, which acquired Civil Maps."
 		},
 		{
-			index: 5,
+			index: 3,
 			title: "Robot Delivery",
 			description: "Worked on developing robot delivery maps, enhancing navigation and efficiency for autonomous delivery systems. This service is now owned by Luminar, which acquired Civil Maps."
 		},
 		{
-			index: 6,
+			index: 2,
 			title: "2D Maps for Uber",
 			description: "As a team manager, I handled the creation and quality control of 2D maps for all cities in the USA, successfully delivering maps for nearly every city across the country."
 		},
 		{
-			index: 7,
+			index: 1,
 			title: "India Maps and POIs",
 			description: "Managed the maps team responsible for creating maps and Points of Interest (POIs) for India, overseeing a team of 80 professionals."
 		}
